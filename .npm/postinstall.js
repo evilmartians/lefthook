8b55d91eed46643a1674bd4ad96fa211a177e159
A.A.Abroskin <arkweid@evilmartians.com>
2019-07-12 19:09:04 +0300

    Remove npx from postinstall script

--- .npm/postinstall.js
+++ .npm/postinstall.js
@@ -1,13 +1,23 @@
 require("process");
 const { spawn } = require("child_process");
+const { join } = require("path");
 
 const isCI = process.env.CI;
 if (!isCI) {
   process.chdir(process.env.INIT_CWD);
 
-  result = spawn("npx", ["lefthook", "install"]);
+  if (process.platform === 'darwin') {
+    binary = 'lefthook-mac';
+  } else if (process.platform === 'linux') {
+      binary = 'lefthook-linux';
+  } else if (process.platform === 'win32') {
+      binary = 'lefthook-win.exe';
+  } else {
+    console.log("Unsupported OS");
+    process.exit(0);
+  }
 
-  result.stdout.on("data", data => {
-    console.log(`${data}`);
-  });
+  binpath = join(process.cwd(), 'node_modules', '@arkweid', 'lefthook', 'bin', binary);
+
+  result = spawn(binpath, ["install", "-f"], { stdio: [process.stdin, process.stdout, process.stderr] });
 }
