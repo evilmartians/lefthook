2a0debaba8bac73aa96b2b3ff6bd081cb1686d0f
Karl Fleischmann <fleischmann.karl@gmail.com>
2019-08-02 13:12:51 +0200

    Download binary on npm postinstall hook
    Try to fetch the binary files on postinstall for each supoorted OS and
    if not CI. This should reduce the npm package size (as you only ever
    download one binary for your host system).

--- .npm/postinstall.js
+++ .npm/postinstall.js
@@ -1,23 +1,55 @@
 require("process");
 const { spawn } = require("child_process");
+const { createWriteStream, unlink } = require("fs");
+const { get } = require("https");
 const { join } = require("path");
+
+const downloadPaths = {
+  "darwin": "https://raw.githubusercontent.com/Arkweid/lefthook/master/.npm/bin/lefthook-mac",
+  "linux": "https://raw.githubusercontent.com/Arkweid/lefthook/master/.npm/bin/lefthook-linux",
+  "win32": "https://raw.githubusercontent.com/Arkweid/lefthook/master/.npm/bin/lefthook-win.exe"
+};
+
+function downloadBinary(os, callback) {
+  // construct single binary file path
+  const binaryPath = join(process.cwd(), 'node_modules', '@arkweid', 'lefthook', 'bin', binary);
+
+  // create write stream as file with the constructed binaryPath, set it
+  // up to close as soon as no more data is expected
+  const file = createWriteStream(binaryPath, { mode: 0o755 });
+  file.on("finish", () => {
+    file.close();
+  });
+
+  // start download of binary file, set it up to remove the file if an
+  // error occurs
+  const download = get(downloadPaths[os], response => {
+    response.pipe(file);
+    response.on("end", () => {
+      callback(binaryPath);
+    });
+  });
+  download.on("error", err => {
+    console.error("Unable to download lefthook binary file");
+    console.error(err.message);
+    unlink(file);
+  });
+}
+
+function installGitHooks(binaryPath) {
+  console.log("Installing git hooks");
+  spawn(binaryPath, ["install", "-f"], { stdio: [process.stdin, process.stdout, process.stderr] });
+}
 
 const isCI = process.env.CI;
 if (!isCI) {
   process.chdir(process.env.INIT_CWD);
 
-  if (process.platform === 'darwin') {
-    binary = 'lefthook-mac';
-  } else if (process.platform === 'linux') {
-      binary = 'lefthook-linux';
-  } else if (process.platform === 'win32') {
-      binary = 'lefthook-win.exe';
-  } else {
+  if (!["darwin", "linux", "win32"].includes(process.platform)) {
     console.log("Unsupported OS");
     process.exit(0);
   }
 
-  binpath = join(process.cwd(), 'node_modules', '@arkweid', 'lefthook', 'bin', binary);
-
-  result = spawn(binpath, ["install", "-f"], { stdio: [process.stdin, process.stdout, process.stderr] });
+  console.log("Downloading `lefthook` binary");
+  downloadBinary(process.platform, installGitHooks);
 }
