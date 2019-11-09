6c49bb00727d85dbb90169c0f86473ce7c8ebaf8
Alexander Abroskin <arkweid@evilmartians.com>
2019-11-09 22:47:35 +0300

    revert postinstall

--- .npm/postinstall.js
+++ .npm/postinstall.js
@@ -1,9 +1,11 @@
+require("process");
 const { spawn } = require("child_process");
 const { join } = require("path");
 
 const isCI = process.env.CI;
-
 if (!isCI) {
+  process.chdir(process.env.INIT_CWD);
+
   if (process.platform === 'darwin') {
     binary = 'lefthook-mac';
   } else if (process.platform === 'linux') {
@@ -15,7 +17,7 @@ if (!isCI) {
     process.exit(0);
   }
 
-  binpath = join(__dirname, 'bin', binary);
+  binpath = join(process.cwd(), 'node_modules', '@arkweid', 'lefthook', 'bin', binary);
 
   result = spawn(binpath, ["install", "-f"], { stdio: [process.stdin, process.stdout, process.stderr] });
 }
