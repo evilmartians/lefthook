97e7957d7f457c8a3f5235e09e5106b51c817c35
Patryk Mirecki <patryk.mirecki@packhelp.com>
2020-05-13 14:46:23 +0200

    refactor: remove-excess-code

--- cmd/root.go
+++ cmd/root.go
@@ -135,19 +135,11 @@ func check(e error) {
 }
 
 func isConfigExtends() bool {
-    if len(viper.GetStringSlice(configExtendsOption)) > 0 {
-      return true
-    }
-	return false
+	return len(viper.GetStringSlice(configExtendsOption)) > 0
 }
 
 func getExtendsPath() []string {
-	extends := make([]string, 0, 10)
-	slices := viper.GetStringSlice(configExtendsOption)
-	for _, v := range slices {
-		extends = append(extends, v)
-	}
-	return extends
+	return viper.GetStringSlice(configExtendsOption)
 }
 
 // EnableColors shows is colors supported for current output or not.
