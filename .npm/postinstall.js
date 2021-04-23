7b44ebfb4fb8d77f0fe90924103de94bf24a0fd2
Andrey Novikov <envek@envek.name>
2021-04-23 13:48:15 +0300

    Package binaries for various architectures to gem and npm packages (#168)

--- .npm/postinstall.js
+++ .npm/postinstall.js
@@ -4,18 +4,7 @@ const { join } = require("path");
 const isCI = process.env.CI;
 
 if (!isCI) {
-  if (process.platform === 'darwin') {
-    binary = 'lefthook-mac';
-  } else if (process.platform === 'linux') {
-      binary = 'lefthook-linux';
-  } else if (process.platform === 'win32') {
-      binary = 'lefthook-win.exe';
-  } else {
-    console.log("Unsupported OS");
-    process.exit(0);
-  }
-
-  binpath = join(__dirname, 'bin', binary);
+  binpath = join(__dirname, 'bin', 'lefthook');
 
   result = spawn(binpath, ["install", "-f"], {
       cwd: process.env.INIT_CWD,
