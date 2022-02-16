794eb1addb2a0063365b2d376c778512a1cd20e2
Valentin Kiselev <mrexox@evilmartians.com>
2022-02-16 14:43:58 +0300

    Embed documentation from a file
    Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

--- pkg/cmd/add.go
+++ pkg/cmd/add.go
@@ -1,31 +1,23 @@
 package cmd
 
 import (
-	"github.com/MakeNowJust/heredoc"
+	_ "embed"
+
 	"github.com/spf13/cobra"
 
 	"github.com/evilmartians/lefthook/pkg/lefthook"
 )
 
+//go:embed add-doc.txt
+var addDoc string
+
 func NewAddCmd(opts *lefthook.Options) *cobra.Command {
 	args := lefthook.AddArgs{}
 
 	addCmd := cobra.Command{
-		Use:   "add hook-name",
-		Short: "This command add a hook directory to a repository",
-		Long: heredoc.Doc(`
-       	This command will try to build the following structure in repository:
-       	├───.git
-       	│   └───hooks
-       	│       └───pre-commit // this executable will be added. Existed file with
-       	│                      // same name will be renamed to pre-commit.old
-       	(lefthook add this dirs if you run command with -d option)
-       	│
-       	├───.lefthook          // directory for project level hooks
-       	│   └───pre-commit     // directory with hooks executables
-       	├───.lefthook-local    // directory for personal hooks add it in .gitignore
-       	│   └───pre-commit
-    `),
+		Use:     "add hook-name",
+		Short:   "This command add a hook directory to a repository",
+		Long:    addDoc,
 		Example: "lefthook add pre-commit",
 		Args:    cobra.MinimumNArgs(1),
 		RunE: func(_cmd *cobra.Command, hooks []string) error {
