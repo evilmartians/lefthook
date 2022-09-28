evilmartians/lefthook

commit f91d56fd968a54bc226f2ba245c57510bcb00f34

fix: Windows escaping issues (#314)
* fix: Windows escaping issue

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

* feature: Wrap every file in quotes if they are provided

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

* fix: Fix shellquote, replace quotes if explicitly set

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

* docs: Write about quoting of shorthands

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

* test: Add a test for replaceQuoted

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

Signed-off-by: Valentin Kiselev <mrexox@evilmartians.com>

internal/lefthook/runner/runner.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"strings"
 	"sync"
@@ -23,6 +24,8 @@ const (
 	executableFileMode os.FileMode = 0o751
 	executableMask     os.FileMode = 0o111
 )
+
+var surroundingQuotesRegexp = regexp.MustCompile(`^'(.*)'$`)
 
 type Runner struct {
 	fs          afero.Fs
@@ -284,14 +287,12 @@ func (r *Runner) buildCommandArgs(command *config.Command) []string {
 				return nil
 			}
 
-			filesStr := prepareFiles(command, files)
-			if len(filesStr) == 0 {
+			filesPrepared := prepareFiles(command, files)
+			if len(filesPrepared) == 0 {
 				return nil
 			}
 
-			runString = strings.ReplaceAll(
-				runString, filesType, filesStr,
-			)
+			runString = replaceQuoted(runString, filesType, filesPrepared)
 		}
 	}
 
@@ -300,12 +301,14 @@ func (r *Runner) buildCommandArgs(command *config.Command) []string {
 		runString = strings.ReplaceAll(runString, fmt.Sprintf("{%d}", i+1), gitArg)
 	}
 
+	log.Debug("Executing command is: ", runString)
+
 	return strings.Split(runString, " ")
 }
 
-func prepareFiles(command *config.Command, files []string) string {
+func prepareFiles(command *config.Command, files []string) []string {
 	if files == nil {
-		return ""
+		return []string{}
 	}
 
 	log.Debug("Files before filters:\n", files)
@@ -323,11 +326,39 @@ func prepareFiles(command *config.Command, files []string) string {
 			filesEsc = append(filesEsc, shellescape.Quote(fileName))
 		}
 	}
-	files = filesEsc
 
-	log.Debug("Files after escaping:\n", files)
+	log.Debug("Files after escaping:\n", filesEsc)
 
-	return strings.Join(files, " ")
+	return filesEsc
+}
+
+func replaceQuoted(source, substitution string, files []string) string {
+	for _, elem := range [][]string{
+		{"\"", "\"" + substitution + "\""},
+		{"'", "'" + substitution + "'"},
+		{"", substitution},
+	} {
+		quote := elem[0]
+		sub := elem[1]
+		if !strings.Contains(source, sub) {
+			continue
+		}
+
+		quotedFiles := files
+		if len(quote) != 0 {
+			quotedFiles = make([]string, 0, len(files))
+			for _, fileName := range files {
+				quotedFiles = append(quotedFiles,
+					quote+surroundingQuotesRegexp.ReplaceAllString(fileName, "$1")+quote)
+			}
+		}
+
+		source = strings.ReplaceAll(
+			source, sub, strings.Join(quotedFiles, " "),
+		)
+	}
+
+	return source
 }
 
 func (r *Runner) run(name, root, failText string, args []string) {

internal/lefthook/runner/execute_windows.go
@@ -5,12 +5,18 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+	"syscall"
 )
 
 type CommandExecutor struct{}
 
 func (e CommandExecutor) Execute(root string, args []string) (*bytes.Buffer, error) {
-	command := exec.Command(args[0], args[1:]...)
+	command := exec.Command(args[0])
+	command.SysProcAttr = &syscall.SysProcAttr{
+		CmdLine: strings.Join(args, " "),
+	}
+
 	rootDir, _ := filepath.Abs(root)
 	command.Dir = rootDir
 

internal/lefthook/runner/runner_test.go
@@ -274,3 +274,75 @@ func resultsMatch(a, b []Result) bool {
 
 	return true
 }
+
+func TestReplaceQuoted(t *testing.T) {
+	for i, tt := range [...]struct {
+		name, source, substitution string
+		files                      []string
+		result                     string
+	}{
+		{
+			name:         "without substitutions",
+			source:       "echo",
+			substitution: "{staged_files}",
+			files:        []string{"a", "b"},
+			result:       "echo",
+		},
+		{
+			name:         "with simple substitution",
+			source:       "echo {staged_files}",
+			substitution: "{staged_files}",
+			files:        []string{"test.rb", "README"},
+			result:       "echo test.rb README",
+		},
+		{
+			name:         "with single quoted substitution",
+			source:       "echo '{staged_files}'",
+			substitution: "{staged_files}",
+			files:        []string{"test.rb", "README"},
+			result:       "echo 'test.rb' 'README'",
+		},
+		{
+			name:         "with double quoted substitution",
+			source:       `echo "{staged_files}"`,
+			substitution: "{staged_files}",
+			files:        []string{"test.rb", "README"},
+			result:       `echo "test.rb" "README"`,
+		},
+		{
+			name:         "with escaped files double quoted",
+			source:       `echo "{staged_files}"`,
+			substitution: "{staged_files}",
+			files:        []string{"'test me.rb'", "README"},
+			result:       `echo "test me.rb" "README"`,
+		},
+		{
+			name:         "with escaped files single quoted",
+			source:       "echo '{staged_files}'",
+			substitution: "{staged_files}",
+			files:        []string{"'test me.rb'", "README"},
+			result:       `echo 'test me.rb' 'README'`,
+		},
+		{
+			name:         "with escaped files",
+			source:       "echo {staged_files}",
+			substitution: "{staged_files}",
+			files:        []string{"'test me.rb'", "README"},
+			result:       `echo 'test me.rb' README`,
+		},
+		{
+			name:         "with many substitutions",
+			source:       `echo "{staged_files}" {staged_files}`,
+			substitution: "{staged_files}",
+			files:        []string{"'test me.rb'", "README"},
+			result:       `echo "test me.rb" "README" 'test me.rb' README`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d: %s", i, tt.name), func(t *testing.T) {
+			result := replaceQuoted(tt.source, tt.substitution, tt.files)
+			if result != tt.result {
+				t.Errorf("Expected `%s` to eq `%s`", result, tt.result)
+			}
+		})
+	}
+}

commit 00831ee18be178333d1581b2978579e606ce5cb7

fix: Regression from #314 (chmod missed fix) (#330)
* Fix regression from #314 (chmod missed fix)

#314 introduced a [regression](https://github.com/evilmartians/lefthook/pull/314#issuecomment-1230927802) that passes quoted paths to `fs.chmod`.
[`fs.Chmod`/`os.chmod` on windows](https://cs.opensource.google/go/go/+/master:src/syscall/syscall_windows.go;l=647;drc=242adb784cd64265ce803f6b0c59dbf126bcda9c) calls into the [`GetFileAttributes`](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfileattributesw)/[`SetFileAttributes`](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-setfileattributesw) win32 api directly. The win32 api expects a "strongly typed" path, without quotes, unlike a command line string, and doesn't try to *parse* it. Passing quoted paths to fs.Chmod causes the operation to fail - valid paths do not begin with quotes!

This change moves the path quoting down into `runScript`, so that we can pass the UNquoted path to `fs.Chmod`.

To my great surprise, it looks like go does NOT check `GetLastError` when `GetFileAttributes`/`SetFileAttributes` fails? This is disappointing, because somewhere down the stack, procmon says windows returns "NAME INVALID", which is probably `STATUS_OBJECT_NAME_INVALID`. Poking around kernelbase.dll in GHIDRA shows a couple places where it sets last error, and you can see plenty more in the ReactOS sources.

* remove unused dir param.

* Move `quotedScriptPath` initialization near actual use

Believe it or not, I'm NOT a C89 programmer, I hate code that initializes variables at the top of the block. And yet, here I am. *Something something cobblers kids have no shoes something*

internal/lefthook/runner/runner.go
@@ -161,23 +161,23 @@ func (r *Runner) runScripts(dir string) {
 			continue
 		}
 
-		scriptPath := shellescape.Quote(filepath.Join(dir, file.Name()))
+		unquotedScriptPath := filepath.Join(dir, file.Name())
 
 		if r.hook.Parallel {
 			wg.Add(1)
 			go func(script *config.Script, path string, file os.FileInfo) {
 				defer wg.Done()
 				r.runScript(script, path, file)
-			}(script, scriptPath, file)
+			}(script, unquotedScriptPath, file)
 		} else {
-			r.runScript(script, scriptPath, file)
+			r.runScript(script, unquotedScriptPath, file)
 		}
 	}
 
 	wg.Wait()
 }
 
-func (r *Runner) runScript(script *config.Script, path string, file os.FileInfo) {
+func (r *Runner) runScript(script *config.Script, unquotedPath string, file os.FileInfo) {
 	if script.DoSkip(r.repo.State()) {
 		logSkip(file.Name(), "(SKIP BY SETTINGS)")
 		return
@@ -196,7 +196,7 @@ func (r *Runner) runScript(script *config.Script, path string, file os.FileInfo)
 
 	// Make sure file is executable
 	if (file.Mode() & executableMask) == 0 {
-		if err := r.fs.Chmod(path, executableFileMode); err != nil {
+		if err := r.fs.Chmod(unquotedPath, executableFileMode); err != nil {
 			log.Errorf("Couldn't change file mode to make file executable: %s", err)
 			r.fail(file.Name(), "")
 			return
@@ -208,7 +208,8 @@ func (r *Runner) runScript(script *config.Script, path string, file os.FileInfo)
 		args = strings.Split(script.Runner, " ")
 	}
 
-	args = append(args, path)
+	quotedScriptPath := shellescape.Quote(unquotedPath)
+	args = append(args, quotedScriptPath)
 	args = append(args, r.args[:]...)
 
 	r.run(RunOptions{