abe96c309d2b1255e0943d6c90f3e20329b6000b
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-15 13:49:53 +0500

    perf(`npm/@evilmartians/lefthook`): update `binary` at postinstall

--- packaging/npm-bundled/get-exe.js
+++ packaging/npm-bundled/get-exe.js
@@ -1,38 +1,39 @@
-const path = require("path")
+const path = require("path");
 
-function getExePath() {
+function getRelativeExePath() {
   // Detect OS
   // https://nodejs.org/api/process.html#process_process_platform
   let goOS = process.platform;
-  let extension = '';
-  if (['win32', 'cygwin'].includes(process.platform)) {
-    goOS = 'windows';
-    extension = '.exe';
+  let extension = "";
+  if (["win32", "cygwin"].includes(process.platform)) {
+    goOS = "windows";
+    extension = ".exe";
   }
 
   // Detect architecture
   // https://nodejs.org/api/process.html#process_process_arch
   let goArch = process.arch;
-  let suffix = '';
+  let suffix = "";
   switch (process.arch) {
-    case 'x64': {
-      goArch = 'amd64';
-      suffix = '_v1'; // GOAMD64
+    case "x64": {
+      goArch = "amd64";
+      suffix = "_v1"; // GOAMD64
       break;
     }
-    case 'x32':
-    case 'ia32': {
-      goArch = '386';
+    case "x32":
+    case "ia32": {
+      goArch = "386";
       break;
     }
   }
 
-  const dir = path.join(__dirname, 'bin');
-  const executable = path.join(
-    dir,
-    `lefthook_${goOS}_${goArch}${suffix}`,
-    `lefthook${extension}`
-  );
+  return [`lefthook_${goOS}_${goArch}${suffix}`, `lefthook${extension}`];
+}
+function getExePath() {
+  const dir = path.join(__dirname, "bin");
+  const [folder, file] = getRelativeExePath();
+  const executable = path.join(dir, folder, file);
   return executable;
 }
+exports.getRelativeExePath = getRelativeExePath;
 exports.getExePath = getExePath;
--- packaging/npm-bundled/postinstall.js
+++ packaging/npm-bundled/postinstall.js
@@ -1,10 +1,27 @@
 if (!process.env.CI) {
-  const { spawnSync } = require('child_process');
-  const { getExePath } = require('./get-exe');
+  const { spawnSync } = require("child_process");
+  const { getExePath, getRelativeExePath } = require("./get-exe");
+  const { writeFileSync } = require("fs");
+  const path = require("path");
+  const pkg = require("./package.json");
 
   // run install
-  spawnSync(getExePath(), ['install', '-f'], {
+  spawnSync(getExePath(), ["install", "-f"], {
     cwd: process.env.INIT_CWD || process.cwd(),
-    stdio: 'inherit',
+    stdio: "inherit",
   });
+
+  writeFileSync(
+    path.resolve(__dirname, "package.json"),
+    JSON.stringify(
+      {
+        ...pkg,
+        bin: {
+          dprint: "bin/" + getRelativeExePath().join("/"),
+        },
+      },
+      null,
+      2,
+    ),
+  );
 }
