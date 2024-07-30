60fc11ee9050d973930e8b70c8961c8e8223a044
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-07-31 01:13:31 +0500

    fix: typo fix for variable name

--- packaging/npm/lefthook/bin.sh
+++ packaging/npm/lefthook/bin.sh
@@ -19,32 +19,32 @@ fi
 
 # Search and find binary
 if $IS_X_CALL; then
-  BIOME_BIN=$(find "${CACHE_DIR}" -iname "${BINARY_NAME}" | grep -s "${BINARY_NAME_SCOPE}" || echo "")
+  LEFTHOOK_BIN=$(find "${CACHE_DIR}" -iname "${BINARY_NAME}" | grep -s "${BINARY_NAME_SCOPE}" || echo "")
 else
-  BIOME_BIN=$(find . -iname "${BINARY_NAME}" | grep -s "${BINARY_NAME_SCOPE}" || echo "")
+  LEFTHOOK_BIN=$(find . -iname "${BINARY_NAME}" | grep -s "${BINARY_NAME_SCOPE}" || echo "")
 fi
 
 # Check node_modules
-if [ -z "${BIOME_BIN}" ]; then
+if [ -z "${LEFTHOOK_BIN}" ]; then
   echo "\`node_modules\` was not installed"
   exit 1
 fi
 
 # Trim variables after success checks
-BIOME_BIN=$(realpath -q "${BIOME_BIN}")
+LEFTHOOK_BIN=$(realpath -q "${LEFTHOOK_BIN}")
 
 # Make it executable
-if test -f "${BIOME_BIN}"; then
-  chmod +x "${BIOME_BIN}"
+if test -f "${LEFTHOOK_BIN}"; then
+  chmod +x "${LEFTHOOK_BIN}"
 fi
 
 # Replace binary in `bin` field for later use
 if test -f "${CURRENT_DIR}/package.json"; then
-  sed -i.bak "s|bin.sh|${BIOME_BIN}|g" "${CURRENT_DIR}/package.json"
+  sed -i.bak "s|bin.sh|${LEFTHOOK_BIN}|g" "${CURRENT_DIR}/package.json"
   rm -rf "package.json.bak"
 elif echo "${CURRENT_DIR}" | grep -q ".bin"; then
-  ln -sf "${BIOME_BIN}" "$0"
+  ln -sf "${LEFTHOOK_BIN}" "$0"
 fi
 
 # Run currently until next run
-"${BIOME_BIN}" "$@"
+"${LEFTHOOK_BIN}" "$@"
