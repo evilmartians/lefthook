8e221cf1e9866921f5e9e0c076ab30a1c1f58a02
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2024-10-25 13:02:21 -0300

    feat: add refetch_frequency parameter to settings

--- docs/configuration.md
+++ docs/configuration.md
@@ -33,6 +33,7 @@ Config options:
   - [`git_url`](#git_url)
   - [`ref`](#ref-1)
   - [`refetch`](#refetch)
+  - [`refetch_frequency`](#refetch_frequency)
   - [`configs`](#configs)
 - [`<pre-commit>`](#hook-name) hook name
   - [`files` (global)](#files-global)
@@ -416,6 +417,29 @@ remotes:
   - git_url: https://github.com/evilmartians/lefthook
     refetch: true
 ```
+
+### `refetch_frequency`
+
+**Default:** Not set
+
+Specifies how frequently Lefthook should refetch the remote configuration. This can be set to `always`, `never` or a time duration like `24h`, `30m`, etc.
+
+- When set to `always`, Lefthook will always refetch the remote configuration on each run.
+- When set to a duration (e.g., `24h`), Lefthook will check the last fetch time and refetch the configuration only if the specified amount of time has passed.
+- When set to `never` or not set, Lefthook will not fetch from remote.
+
+**Example**
+
+```yml
+# lefthook.yml
+
+remotes:
+  - git_url: https://github.com/evilmartians/lefthook
+    refetch_frequency: 24h # Refetches once every 24 hours
+```
+
+> [!WARNING]
+> If `refetch` is set to `true`, it overrides any setting in `refetch_frequency`.
 
 ### `configs`
 
--- internal/config/remote.go
+++ internal/config/remote.go
@@ -4,9 +4,10 @@ type Remote struct {
 	GitURL string `json:"git_url,omitempty" mapstructure:"git_url"       toml:"git_url"       yaml:"git_url"`
 	Ref    string `json:"ref,omitempty"     mapstructure:"ref,omitempty" toml:"ref,omitempty" yaml:",omitempty"`
 	// Deprecated
-	Config  string   `json:"config,omitempty"  mapstructure:"config,omitempty"  toml:"config,omitempty"  yaml:",omitempty"`
-	Configs []string `json:"configs,omitempty" mapstructure:"configs,omitempty" toml:"configs,omitempty" yaml:",omitempty"`
-	Refetch bool     `json:"refetch,omitempty" mapstructure:"refetch,omitempty" toml:"refetch,omitempty" yaml:",omitempty"`
+	Config           string   `json:"config,omitempty"            mapstructure:"config,omitempty"            toml:"config,omitempty"            yaml:",omitempty"`
+	Configs          []string `json:"configs,omitempty"           mapstructure:"configs,omitempty"           toml:"configs,omitempty"           yaml:",omitempty"`
+	Refetch          bool     `json:"refetch,omitempty"           mapstructure:"refetch,omitempty"           toml:"refetch,omitempty"           yaml:",omitempty"`
+	RefetchFrequency string   `json:"refetch_frequency,omitempty" mapstructure:"refetch_frequency,omitempty" toml:"refetch_frequency,omitempty" yaml:",omitempty"`
 }
 
 func (r *Remote) Configured() bool {
