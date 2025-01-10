4e7830951b1097f2295a436ff009926ade82e028
Valentin Kiselev <mrexox@evilmartians.com>
2025-01-10 17:02:30 +0300

    fix: replace cmd in jobs

--- internal/config/load.go
+++ internal/config/load.go
@@ -537,6 +537,17 @@ func mergeJobsSlice(src, dest []interface{}) []interface{} {
 						destSubJobs = mergeJobsSlice(srcSubJobs, destSubJobs)
 					}
 
+					switch srcRun := srcJob["run"].(type) {
+					case string:
+						switch destRun := destJob["run"].(type) {
+						case string:
+							newRun := strings.ReplaceAll(srcRun, CMD, destRun)
+							srcJob["run"] = newRun
+						default:
+						}
+					default:
+					}
+
 					maps.Merge(srcJob, destJob)
 
 					if len(destSubJobs) != 0 {
