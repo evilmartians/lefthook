1ca4d5aa65baa7f926dfaf5f850bae6e34113862
Valentin Kiselev <mrexox@evilmartians.com>
2025-01-14 18:04:14 +0300

    chore: fix comment

--- internal/config/load.go
+++ internal/config/load.go
@@ -114,7 +114,7 @@ func LoadKoanf(filesystem afero.Fs, repo *git.Repository) (*koanf.Koanf, *koanf.
 		return nil, nil, err
 	}
 
-	// Don't allow to set `lefthook` field by a remote config
+	// Don't allow to set `lefthook` field from a remote config
 	secondary.Delete("lefthook")
 
 	// Load optional local config (e.g. lefthook-local.yml)
