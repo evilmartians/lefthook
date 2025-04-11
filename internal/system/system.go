10ba801713601d512061b2c4d1a57f7693b0fcd8
Valentin Kiselev <mrexox@evilmartians.com>
2025-04-11 11:19:28 +0300

    chore: remove comments

--- internal/system/system.go
+++ internal/system/system.go
@@ -70,15 +70,5 @@ func (c osCmd) RunWithContext(
 
 	err := cmd.Run()
 
-	// b := log.Builder(log.DebugLevel)
-	// b.Add("[lefthook] cmd:    ", strings.Join(command, " "))
-	// if len(root) > 0 {
-	// 	b.Add("[lefthook] dir:    ", root)
-	// }
-	// if err != nil {
-	// 	b.Add("[lefthook] error:  ", err)
-	// }
-	// b.Log()
-
 	return err
 }
