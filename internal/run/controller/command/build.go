54caac5c59ccb2d744f7888adb2aaea572f5e5f1
Valentin Kiselev <mrexox@evilmartians.com>
2025-09-23 15:36:57 +0300

    fix: allow skipping groups (#1140)

--- internal/run/controller/command/build.go
+++ internal/run/controller/command/build.go
@@ -3,8 +3,6 @@ package command
 import (
 	"github.com/evilmartians/lefthook/internal/config"
 	"github.com/evilmartians/lefthook/internal/git"
-	"github.com/evilmartians/lefthook/internal/run/controller/utils"
-	"github.com/evilmartians/lefthook/internal/system"
 )
 
 type JobParams struct {
@@ -23,14 +21,12 @@ type JobParams struct {
 }
 
 type BuilderOptions struct {
-	HookName    string
-	ExcludeTags []string
-	GitArgs     []string
-	ForceFiles  []string
-	SourceDirs  []string
-	OnlyTags    []string
-	Templates   map[string]string
-	Force       bool
+	HookName   string
+	GitArgs    []string
+	ForceFiles []string
+	SourceDirs []string
+	Templates  map[string]string
+	Force      bool
 }
 
 type Builder struct {
@@ -47,36 +43,11 @@ func NewBuilder(repo *git.Repository, opts BuilderOptions) *Builder {
 
 // BuildCommands returns the list of commands and the list of files touched by the command.
 func (b *Builder) BuildCommands(params *JobParams) ([]string, []string, error) {
-	if reason := b.skipReason(params); len(reason) > 0 {
-		return nil, nil, SkipError{reason}
-	}
-
 	if len(params.Run) != 0 {
 		return b.buildCommand(params)
 	} else {
 		return b.buildScript(params)
 	}
-}
-
-func (b *Builder) skipReason(params *JobParams) string {
-	skipChecker := config.NewSkipChecker(system.Cmd)
-	if skipChecker.Check(b.git.State, params.Skip, params.Only) {
-		return "by condition"
-	}
-
-	if len(b.opts.OnlyTags) > 0 && !utils.Intersect(b.opts.OnlyTags, params.Tags) {
-		return "tags"
-	}
-
-	if utils.Intersect(b.opts.ExcludeTags, params.Tags) {
-		return "tags"
-	}
-
-	if utils.Intersect(b.opts.ExcludeTags, []string{params.Name}) {
-		return "name"
-	}
-
-	return ""
 }
 
 func (p *JobParams) validateCommand() error {
