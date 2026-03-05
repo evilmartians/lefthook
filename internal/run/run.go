a9cb6568acb08dfde150b9753afe815abb030e84
Valentin Kiselev <mrexox@evilmartians.com>
2026-03-05 16:42:40 +0300

    chore: small code improvements (#1336)

--- internal/run/run.go
+++ internal/run/run.go
@@ -9,10 +9,20 @@ import (
 	"github.com/evilmartians/lefthook/v2/internal/run/result"
 )
 
+// FailOnChangesError is a special error that fails the hook if any project file was changed.
+//
+// Exported here to be handled separately on the caller side.
 type FailOnChangesError = controller.FailOnChangesError
 
+// Options contain hook arguments and special execution settings.
 type Options = controller.Options
 
-func Run(ctx context.Context, hook *config.Hook, repo *git.Repository, opts Options) ([]result.Result, error) {
+// Run executes the hook.
+func Run(
+	ctx context.Context,
+	hook *config.Hook,
+	repo *git.Repository,
+	opts Options,
+) ([]result.Result, error) {
 	return controller.NewController(repo).RunHook(ctx, opts, hook)
 }
